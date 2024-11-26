5854be8dc6c7ba5a2b919ea9095e5e8bfefbecd5
naize000 <baket123@bk.ru>
2024-11-26 22:44:56 +0300

    0,5

--- js/index.js
+++ js/index.js
@@ -13,15 +13,16 @@ $('.single-item').slick({
     swipe: true,
   });
 
-  $('.reviews').slick({
-    dots: true,
-    infinite: true,
-    speed: 100,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    fade: true,
-    arrows: false,
-    swipe: true,
-  });
+  $('.scrollto a').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, {
+        duration: 370,   // по умолчанию «400» 
+        easing: "linear" // по умолчанию «swing» 
+    });
+
+    return false;
+});
