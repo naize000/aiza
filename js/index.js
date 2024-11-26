5c730d7ddf0c896b644966fe634e365f9dcc98dc
naize000 <baket123@bk.ru>
2024-11-26 18:40:38 +0300

    первый готовый вариант frontend
    0.4

--- js/index.js
+++ js/index.js
@@ -9,5 +9,19 @@ $('.single-item').slick({
     autoplay: true,
     autoplaySpeed: 4000,
     fade: true,
-    arrows: false
+    arrows: false,
+    swipe: true,
+  });
+
+  $('.reviews').slick({
+    dots: true,
+    infinite: true,
+    speed: 100,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    arrows: false,
+    swipe: true,
   });
